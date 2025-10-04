caa8d99f54ad875ef1a263c6bf4745acb78950f0
aishwaryadixit <aishwaryadixit2495@gmail.com>
2025-10-04 20:57:20 +0000

    fixed tf fmt

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-variable "ARM_SUBSCRIPTION_ID" {  type = string }
+variable "ARM_SUBSCRIPTION_ID" { type = string }
 
-variable "ARM_TENANT_ID" {  type = string }
+variable "ARM_TENANT_ID" { type = string }
 
-variable "ARM_CLIENT_ID" {  type = string }
+variable "ARM_CLIENT_ID" { type = string }
 
-variable "ARM_CLIENT_SECRET" {  type = string }
+variable "ARM_CLIENT_SECRET" { type = string }
 
 terraform {
   required_version = ">=1.3.0"
@@ -25,10 +25,10 @@ terraform {
 provider "azurerm" {
   features {}
   skip_provider_registration = true
-  subscription_id = var.ARM_SUBSCRIPTION_ID
-  tenant_id = var.ARM_TENANT_ID
-  client_id = var.ARM_CLIENT_ID
-client_secret = var.ARM_CLIENT_SECRET
+  subscription_id            = var.ARM_SUBSCRIPTION_ID
+  tenant_id                  = var.ARM_TENANT_ID
+  client_id                  = var.ARM_CLIENT_ID
+  client_secret              = var.ARM_CLIENT_SECRET
 }
 
 resource "azurerm_resource_group" "rg" {
